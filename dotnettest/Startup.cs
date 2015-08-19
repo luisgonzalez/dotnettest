e5ce9c2e658babfb1d739d066e02196dc3488717
Luis Gonzalez <lgonzalez@foundationmedicine.com>
2015-08-19 11:53:35 -0500

    Something something

---
+++ dotnettest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dotnettest.Startup))]
+namespace dotnettest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
